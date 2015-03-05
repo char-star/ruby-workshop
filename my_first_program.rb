001606ba80b1b282d734ee2ee77cd0097925a68a
Satish Perala <satish@cybrilla.com>
2015-03-05 15:08:50 +0530

    Removed unwanted line

--- my_first_program.rb
+++ my_first_program.rb
@@ -2,6 +2,5 @@ first_name = "Satish"
 last_name = "Perala"
 fav_color = "Red"
 
-puts "Hi, my name is first_name. My fav color is fav_color"
 
 puts "Hi, my name is " + first_name + ". My fav color is " + fav_color
