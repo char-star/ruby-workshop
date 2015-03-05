344896e96b0b9d7b42640540ff0fae3d9e64706f
Satish Perala <satish@cybrilla.com>
2015-03-05 14:58:11 +0530

    added string concatination

---
+++ my_first_program.rb
@@ -0,0 +1,7 @@
+first_name = "Satish"
+last_name = "Perala"
+fav_color = "Red"
+
+puts "Hi, my name is first_name. My fav color is fav_color"
+
+puts "Hi, my name is " + first_name + ". My fav color is " + fav_color
